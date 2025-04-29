6a868298b7e90ce894d14a2275a074ecbadf4325
ananya matta <106088301+okayyananya@users.noreply.github.com>
2025-04-29 16:23:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def predict():
     return render_template('index.html', prediction=label, user_text=text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
